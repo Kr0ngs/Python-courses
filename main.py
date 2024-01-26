6d27838e3421d86f6f6c7611ef2ac181ff60065f
Aleksandr <sasha.skvortsov.04@inbox.ru>
2024-01-26 21:00:48 +0300

    8_Project

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-def nums_info(a, b):
-    if (type(a) is not int) or (type(b) is not int):
-        return "Один из аргументов не целое число"
-
-    if a >= b:
-        return f"{a} больше или равно {b}"
-
-    return f"{a} меньше {b}"
-
-
-print(nums_info(True, 10))
-
-print(nums_info(10, 2))
-print(nums_info(4, 15))
+my_img = ('asdasjamsdjasdnmaiosjdjqaiojlkasjlkdajlkskjldalkjsdjlkaswd')
+print(f"Str a long") if len(my_img) >= 72 else print("String a short")
---
+++ Задача номер 8(Тернарный оператор).py
@@ -0,0 +1,2 @@
+my_img = ('I have an apartment in Kazan.')
+print(f"Str a long") if len(my_img) >= 72 else print("String a short")
